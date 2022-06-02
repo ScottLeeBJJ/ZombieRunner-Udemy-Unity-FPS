2f5c7955046e34e9306fccd0bbf359a1cfa46951
ScottLeeBJJ <scottlee5455@outlook.com>
2022-06-01 23:05:23 -0700

    Working on different weapons having different ammo types and amounts

--- ZombieRunner/Assets/Scripts/Ammo.cs
+++ ZombieRunner/Assets/Scripts/Ammo.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
-    [SerializeField] ammoSlot[] ammoSlot;
-    
+    [SerializeField] AmmoSlot[] ammoSlots;
+
     [System.Serializable]
-    private class ammoSlot
+    private class AmmoSlot
     {
         public AmmoType ammoType;
         public int ammoAmount;
     }
 
-    public int GetCurrentAmmo()
+    public int GetCurrentAmmo(AmmoType ammoType)
     {
-        return ammoAmount;
+        return GetAmmoSlot(ammoType).ammoAmount;
     }
 
-    public void ReduceCurrentAmmo()
+    public void ReduceCurrentAmmo(AmmoType ammoType)
     {
-        ammoAmount --;
+        GetAmmoSlot(ammoType).ammoAmount--;
     }
+
+    private AmmoSlot GetAmmoSlot(AmmoType ammoType)
+    {
+        foreach (AmmoSlot slot in ammoSlots)
+        {
+            if (slot.ammoType == ammoType)
+            {
+                return slot;
+            }
+        }
+        return null;
+    }
+
 }
--- ZombieRunner/Assets/Scripts/Weapon.cs
+++ ZombieRunner/Assets/Scripts/Weapon.cs
@@ -1,20 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
-{    
+{
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
     [SerializeField] Ammo ammoSlot;
+    [SerializeField] AmmoType ammoType;
     [SerializeField] float timeBetweenShots = 0.5f;
 
     bool canShoot = true;
 
-    private void OnEnable() 
+    private void OnEnable()
     {
         canShoot = true;
     }
@@ -30,22 +32,22 @@ public class Weapon : MonoBehaviour
     IEnumerator Shoot()
     {
         canShoot = false;
-        if (ammoSlot.GetCurrentAmmo() > 0)
+        if (ammoSlot.GetCurrentAmmo(ammoType) > 0)
         {
             PlayMuzzleFlash();
             ProcessRaycast();
-            ammoSlot.ReduceCurrentAmmo();
+            ammoSlot.ReduceCurrentAmmo(ammoType);
         }
         yield return new WaitForSeconds(timeBetweenShots);
         canShoot = true;
     }
 
-    void PlayMuzzleFlash()
+    private void PlayMuzzleFlash()
     {
         muzzleFlash.Play();
     }
 
-    void ProcessRaycast()
+    private void ProcessRaycast()
     {
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
@@ -61,7 +63,7 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    void CreateHitImpact(RaycastHit hit)
+    private void CreateHitImpact(RaycastHit hit)
     {
         GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
         Destroy(impact, .1f);
